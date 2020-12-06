c4127f7b168b4b0458a8b0400afde5b546037933
Dmitry <dimasya.qwerty2015@yandex.ru>
2020-12-06 10:07:16 +0300

    фиксы

--- scripts/Card.js
+++ scripts/Card.js
@@ -2,37 +2,29 @@ export class Card {
     constructor(card, templeteSelector, openImage) {
         this._name = card.name;
         this._link = card.link;
-        this._element = this._getTemplate(templeteSelector);
-        this._getCardElements();
-
+        this._templateSelector = templeteSelector;
         this._openImage = openImage;
     }
-    _getTemplate(template) {
-        return document.querySelector(template)
+    _getTemplate() {
+        return document.querySelector(this._templateSelector)
             .content
             .cloneNode(true);
     }
-    _getCardElements() {
-        this.cardImage = this._element.querySelector('.card__image');
-        this.likeBtn = this._element.querySelector('.card__btn');
-        this.deleteBtn = this._element.querySelector('.card__delete');
-    }
-
     _setEventListeners() {
-        this.likeBtn.addEventListener('click', function(evt) {
+        this._element.querySelector('.card__btn').addEventListener('click', function(evt) {
             evt.target.classList.toggle('card__btn_active');
         });
-        this.deleteBtn.addEventListener('click', function(evt) {
-            const deleteItem = evt.target.closest('.card');
-            deleteItem.remove();
+        this._element.querySelector('.card__delete').addEventListener('click', function(evt) {
+            evt.target.closest('.card').remove();
         });
-        this.cardImage.addEventListener('click', (evt) => {
+        this._element.querySelector('.card__image').addEventListener('click', (evt) => {
             this._openImage(evt);
         });
     }
     generateCard() {
-        this.cardImage.src = this._link;
-        this.cardImage.setAttribute('alt', this._name);
+        this._element = this._getTemplate();
+        this._element.querySelector('.card__image').src = this._link;
+        this._element.querySelector('.card__image').setAttribute('alt', this._name);
         this._element.querySelector('.card__heading').textContent = this._name;
         this._setEventListeners();
         return this._element;
